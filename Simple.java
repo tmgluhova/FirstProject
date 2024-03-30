27db8e06f060e8f96a169be67773990b76c6f8c0
tmgluhova <t.gluhova@psu.by>
2024-03-30 07:45:16 +0300

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java!!! Hello!!!");  
+     System.out.println("Hello Java!!! Hello!!");  
     }  
 }  
